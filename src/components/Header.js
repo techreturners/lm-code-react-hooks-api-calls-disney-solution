1f485866b62659e6121693a584f9d9b8287c1424
James Heggs <james.heggs@techreturners.com>
2021-11-19 22:00:34 +0000

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="header__container">
+      <h1 className="header__title header__welcome">The World of Disney</h1>
+    </header>
+  )
+}
+
+export default Header
